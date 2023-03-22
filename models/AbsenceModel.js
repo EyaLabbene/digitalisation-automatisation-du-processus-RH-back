c71176a4301e146a525c8284732093234bcf10cb
unknown <eyalabbene25@gmail.com>
2023-03-22 19:22:38 +0100

    absence model changed

--- models/AbsenceModel.js
+++ models/AbsenceModel.js
@@ -1,12 +1,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const AbsenceSchema = new Schema(
+const absenceSchema = new Schema( //rit houni? omek ma tabda esm variable b majuscule absenceSchema kelma thenia w kol tabda b majuscule kahaw
   {
-    nom_employee: { type: String, required: true }, //22 nekhdmouhesh haka hedhi fasakha w to nwarik kifeh naamlouha besh naamlou virtual link binet el absence w employé appartir mel id tansesh l esm ynajem yetawed ama id dima unique
+    employee: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "user",
+      // refConditions: {
+      //     $or: [
+      //         { deleted: { $exists: false } },
+      //         { deleted: { $exists: true, $eq: false } },
+      //     ],
+      // },
+    },
     justification: { type: String, required: true },
+    start_date: { type: Date, required: true },
+    end_date: { type: Date },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("absence", AbsenceSchema);
+module.exports = mongoose.model("absence", absenceSchema);
